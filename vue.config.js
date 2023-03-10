1a030dd5eb3acef5dc13521f4ddaabf821c9feeb
jin4333 <vhfhflsksk@gmail.com>
2023-03-10 15:48:33 +0900

    deply build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.Mode_ENV === "production" ? "/test-vue" : "/",
+  outputDir: "docs",
+};
